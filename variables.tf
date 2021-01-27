d8899d39fa88e35e9bf876f2bb904cc4e7c1f672
Dylan King <dylanking@MacBook-Pro.attlocal.net>
2021-01-26 23:47:31 -0600

    remove region var

--- variables.tf
+++ variables.tf
@@ -2,11 +2,6 @@ variable "project_id" {
   type = string
 }
 
-variable "region" {
-  type    = string
-  default = "us-central1"
-}
-
 variable "zone" {
   type    = string
   default = "us-central1-a"
